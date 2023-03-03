b82efc4e530c97e939b994883198def4d6d3cd53
Kennedy Cole <colekennedyt@gmail.com>
2023-03-03 23:24:09 +0000

    Birds

--- src/main/java/Bird.java
+++ src/main/java/Bird.java
@@ -45,9 +45,9 @@ public class Bird {
      *         greater than or equal to 3.
      */
     public String sound(){    
-        if(ageMonths<3){
+        if(ageMonths < 3){
             return "Tweet";
-        }else if(ageMonths>=3){
+        }else if(ageMonths >= 3){
             return "Chirp";
         }
     }
