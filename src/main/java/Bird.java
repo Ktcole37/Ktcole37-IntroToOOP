89acac312f02bb8d38a5530f7de51fea9da8f93f
Kennedy Cole <colekennedyt@gmail.com>
2023-03-07 00:58:48 +0000

    Birds_Class

--- src/main/java/Bird.java
+++ src/main/java/Bird.java
@@ -31,9 +31,9 @@ public class Bird {
     /**
      * A variable (in this context, often referred to as a field), which represents the age of the bird.
      */
-    public String ageMonths;{
-       
-        }
+    public int ageMonths;{
+
+    }
     /**
      * A constructor for the bird class: this method allows us to build a new bird with the 'new' keyword.
      * No need to change anything here, for now.
@@ -46,8 +46,11 @@ public class Bird {
      *         greater than or equal to 3.
      */
     public String sound(){ 
-        return "Chirp";
-
+        if (ageMonths < 3){
+            return "Tweet";
+        }else if (ageMonths >= 3){
+            return "Chirp";
+        }
         }
 
     
